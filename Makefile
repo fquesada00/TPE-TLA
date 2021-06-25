bc1de8581184e85a7c86f5478efe07497739d0b1
OctavioSerpe <octavio.serpe@gmail.com>
2021-06-25 00:16:17 -0300

    added remove/add edge and node functions, passed valgrind and fsanitize
    Co-authored-by: fquesada<fquesada@itba.edu.ar>

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 all:
 	yacc -d C-Graph.y --debug --verbose
 	lex C-Graph.l
-	gcc -o CG lex.yy.c y.tab.c AST.c translateAST.c -lfl -g
+	$(CC) -o CG lex.yy.c y.tab.c AST.c translateAST.c -lfl -g
 
 clean:
 	rm CG y.tab.c y.tab.h lex.yy.c y.output
+
+graph:
+	$(CC) -o graph graph.c  -Wall -pedantic -g
+
+.PHONY: all clean graph
--- graph.c
+++ graph.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
@@ -17,6 +18,7 @@ typedef struct GraphNode{
     struct GraphEdgeListElement * lastEdge;
     struct GraphEdgeListElement * firstEdge;
     size_t size;
+    int id;
 }GraphNode;
 
 
@@ -39,14 +41,17 @@ typedef struct GraphEdgeListElement{
     struct GraphEdgeListElement * next;
 }GraphEdgeListElement;
 
-Graph * graph;
+void breakpoint() {
+    int *p = NULL;
+    printf("%d\n",*p);
+}
 
 Graph * newGraph(){
     Graph * g = calloc(1,sizeof(Graph));
     return g;
 }
 
-/* private */ void addGraphNode(GraphNode * node){
+/* private */ void addGraphNode(Graph * graph, GraphNode * node){
     if(graph->size == 0){
         graph->first = calloc(1,sizeof(GraphNodeListElement));
         graph->first->value = node;
@@ -62,13 +67,15 @@ Graph * newGraph(){
     graph->size++;
 }
 
-GraphNode * newGraphNode(){
+GraphNode * newGraphNode(Graph * graph, int id){
     GraphNode * n = calloc(1,sizeof(GraphNode));
-    n->lastNode = calloc(1,sizeof(GraphNodeListElement));
     n->firstNode = calloc(1,sizeof(GraphNodeListElement));
-    n->lastEdge = calloc(1, sizeof(GraphEdgeListElement));
+    n->lastNode = n->firstNode;
     n->firstEdge = calloc(1, sizeof(GraphEdgeListElement));
-    addGraphNode(n);
+    n->lastEdge = n->firstEdge;
+    n->id = id;
+    addGraphNode(graph, n);
+
     return n;
 }
 
@@ -107,11 +114,9 @@ bool isAdjacent(GraphNode * origin, GraphNode * destination) {
 
 void addEdge(GraphNode * node1, GraphNode * node2, void * value, edgeType type){
     if(node1->size ==0){
-        node1->firstEdge = calloc(1,sizeof(GraphEdgeListElement));
         node1->firstEdge->value = newEdge(value,type);
         node1->firstNode->next=NULL;
         node1->lastEdge = node1->firstEdge;
-        node1->firstNode = calloc(1,sizeof(GraphNodeListElement));
         node1->firstNode->value = node2;
         node1->firstNode->next = NULL;
         node1->lastNode = node1->firstNode;
@@ -122,13 +127,13 @@ void addEdge(GraphNode * node1, GraphNode * node2, void * value, edgeType type){
         return;
     }
     node1->lastEdge->next = calloc(1,sizeof(GraphEdgeListElement));
-    node1->lastEdge->value = newEdge(valloc,type);
+    node1->lastEdge->next->value = newEdge(value,type);
     node1->lastEdge->next->next = NULL;
     node1->lastEdge = node1->lastEdge->next;
     node1->lastNode->next = calloc(1,sizeof(GraphNodeListElement));
     node1->lastNode->next->value = node2;
     node1->lastNode->next->next = NULL;
-    node1->lastNode = node2;
+    node1->lastNode = node1->lastNode->next;
     node1->size++;
 }
 
@@ -139,15 +144,16 @@ void setRoot(GraphNode * node){
 /* private */ void freeEdge(GraphEdge * edge){
     free(edge->weight);
     free(edge);
+    printf("at %s\n", __func__);
 }
 
 /* private */ void freeEdges(GraphNode * node){
-    GraphEdgeListElement * e = node->firstEdge;
-    GraphNodeListElement * n = node->firstNode;
+    GraphEdgeListElement * e = node->firstEdge, * currE = NULL;
+    GraphNodeListElement * n = node->firstNode, * currN = NULL;
     while(e != NULL || n != NULL){
         freeEdge(e->value);
-        GraphEdgeListElement * currE = e;
-        GraphNodeListElement * currN = n;
+        currE = e;
+        currN = n;
         e = e->next;
         n = n->next;
         free(currE);
@@ -155,6 +161,9 @@ void setRoot(GraphNode * node){
     }
 }
 
+/*  
+** al sacar una arista tengo q actualizar la lista del nodo y arista
+*/
 void removeEdge(GraphNode * node1, GraphNode * node2) {
     GraphEdgeListElement * e = node1->firstEdge;
     GraphNodeListElement * n = node1->firstNode;
@@ -167,6 +176,8 @@ void removeEdge(GraphNode * node1, GraphNode * node2) {
         freeEdge(e->value);
         free(e);
         free(n);
+        printf("at %s\n", __func__);
+        node1->size--;
         return;
     }
     while(n != NULL && n->value != node2) {
@@ -179,14 +190,16 @@ void removeEdge(GraphNode * node1, GraphNode * node2) {
         return;
     prevN->next = n->next;
     prevE->next = e->next;
-    freeEdge(n->value);
+    freeEdge(e->value);
     free(n);
     free(e);
+    printf("at %s\n", __func__);
+    node1->size--;
 }
 /*
     * Hay que recorrer todos los nodos del grafo menos el que quiero eliminar y por cada uno eliminar su adyacencia
 */
-/* private */ void removeAdjacency(GraphNode * node){
+/* private */ void removeAdjacency(Graph * graph, GraphNode * node){
     GraphNodeListElement * n = graph->first;
     while(n != NULL){
         if(n->value != node){
@@ -196,13 +209,20 @@ void removeEdge(GraphNode * node1, GraphNode * node2) {
     }
 }
 
-void removeNode(GraphNode * node){
+void removeNode(Graph * graph, GraphNode * node){
     GraphNodeListElement * n = graph->first;
     GraphNodeListElement * prevN = NULL;
     if(n != NULL && n->value == node){
+        if(n == graph->last) {
+            graph->last = prevN;
+        }
+        removeAdjacency(graph, n->value);
         graph->first = n->next;
-        removeAdjacency(n);
+        graph->size--;
+        freeEdges(n->value);
+        free(n->value);
         free(n);
+        printf("at %s\n", __func__);
         return;
     }
 
@@ -210,24 +230,67 @@ void removeNode(GraphNode * node){
         prevN = n;
         n = n->next;
     }
-    if(n == NULL)
+    if(n == NULL){
         return;
+    }
+    if(n == graph->last) {
+        graph->last = prevN;
+    }
+    removeAdjacency(graph,n->value);
     prevN->next = n->next;
-    removeAdjacency(n);
+    graph->size--;
+    freeEdges(n->value);
+    free(n->value);
     free(n);
+    printf("at %s\n", __func__);
 }
 
+void freeGraph(Graph * graph){
+    GraphNodeListElement * it = graph->first, * next;
+
+    while(it != NULL){
+        next = it->next;
+        removeNode(graph, it->value);
+        it = next;
+    }   
+    free(graph);
+    printf("at %s\n", __func__);
+
+}
+
+void printNodeList(GraphNode * node) {
+    GraphEdgeListElement * e = node->firstEdge;
+    GraphNodeListElement * n = node->firstNode;
+    while(e != NULL || n != NULL){
+        printf("%d-%c->%d\n",node->id,*((char*)e->value->weight),n->value->id);
+        e = e->next;
+        n = n->next;
+    }
+    printf("\n\n");
+}
+
+void printGraph(Graph * graph) {
+    GraphNodeListElement * n = graph->first;
+    while(n != NULL){
+        printf("\nPrinting adjacency for node %d\n", n->value->id);
+        printNodeList(n->value);
+        n = n->next;
+    }
+}
+
+
 int main(){
-    graph = newGraph();
+    Graph * graph = newGraph();
     //Node node1, node2, node3, node4, node5
-    GraphNode * node1 = newGraphNode();
-    GraphNode * node2 = newGraphNode();
-    GraphNode * node3 = newGraphNode();
-    GraphNode * node4 = newGraphNode();
-    GraphNode * node5 = newGraphNode();
+    GraphNode * node1 = newGraphNode(graph,1);
+    GraphNode * node2 = newGraphNode(graph, 2);
+    GraphNode * node3 = newGraphNode(graph,3);
+    GraphNode * node4 = newGraphNode(graph,4);
+    GraphNode * node5 = newGraphNode(graph,5);
     //node1-'h'->node2;
-    char a = 'h';//-->Necesary?
+    char a = 'a';//-->Necesary?}
     addEdge(node1,node2,(void *)&a,CHAR_EDGE);
+    addEdge(node1, node3,(void *)&a,CHAR_EDGE);
     //node2-'o'->node3->node4;
     char b = 'h';
     char c = 'l';
@@ -241,10 +304,13 @@ int main(){
     addEdge(node5,node1,(void *)&e,CHAR_EDGE);
     //->node5;
     setRoot(node5);
-    //destroy node5;
-    removeNode(node5);
+    
     //foreach(node in graph)
 
+    printGraph(graph);
 
-
+    // destroy node1
+    removeNode(graph, node1);
+    printGraph(graph);
+    freeGraph(graph);
 }
